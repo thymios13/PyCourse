c4528378610c7552ba23c5e28c3bc47c54002789
Efthymios Maneas <emaneas@efthymioss-mbp.home>
2014-10-31 13:04:09 +0000

    Updated version including a few tests

--- monteCarlo/diffusion_model.py
+++ monteCarlo/diffusion_model.py
@@ -14,12 +14,21 @@ def energy(density, coeff=1.0):
 
     # Make sure input is an array
     density = array(density)
-    
-    # of the right kind (integer). Unless it is zero length, in which case type doesn't matter
-    if density.dtype.kind != 'i' and len(density) > 0:
-        raise TypeError("Expected array of *integers*.")
-    # and the right values (positive or null)
+
+    # Check if input is empty
+    if len(density) < 0:
+        raise TypeError("Expected matrix as input")
+
+    # Check if input is not a "list" type
+    if density.dtype.kind != 'i':
+        raise TypeError("The density matrix is not in the proper form!");
+
+    # Check if the dimensions of the input array are > 1
+    if density.ndim != 1:
+        raise TypeError("The density matrix should be 1-Dimensional!");
+
+    # Check if a value is negative(Only positive or null values are supported)
     if any(density < 0):
-        raise ValueError("Expected array of *positive* integers.")
+        raise ValueError("Expected matrix of positive or null integers")
 
     return coeff * 0.5 * sum(density * (density - 1))
