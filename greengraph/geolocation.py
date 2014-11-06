ae1880e70353ebce504ecd50d257272a3bfc1e8a
Efthymios Maneas <emaneas@efthymioss-mbp.home>
2014-11-06 02:43:02 +0000

    Git Clean up

--- greengraph/geolocation.py
+++
@@ -1,5 +0,0 @@
-import geopy
-geocoder=geopy.geocoders.GoogleV3(domain="maps.google.co.uk")ech
-
-def geolocate(place):
-  return geocoder.geocode(place,exactly_one=False)[0][1]
--- monteCarlo/run.py
+++
@@ -1,4 +0,0 @@
-from diffusion_model import energy
-from monteCarlo import main
-
-main(energy, [1, 2, 3, 2, 4, 5, 3, 2])
